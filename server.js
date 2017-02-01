c5cb477271d479a2365497a62cd8f89b32169510
Sourajit Paul <sourajit.paul61@gmail.com>
2017-02-01 12:45:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article1' ,function(req,res){
+    res.send("article one requested");
+});
+
+app.get('/article2' ,function(req,res){
+    res.send("article two requested");
+});
+
+app.get('/article3' ,function(req,res){
+    res.send("article three requested");
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
